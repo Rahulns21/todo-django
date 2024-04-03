b90e3de84927051dc6d546f8583d0c422dbd0bdf
Rahul <rahul9380767545@gmail.com>
2024-04-03 18:14:24 +0530

    todo app final

--- todo/urls.py
+++ todo/urls.py
@@ -5,4 +5,8 @@ app_name = 'todo'
 urlpatterns = [
     path('', views.home, name='home'),
     path('add-task/', views.add_task, name='add-task'),
+    path('task-complete/<str:slug>/', views.task_complete, name='task-complete'),
+    path('task-undone/<str:slug>/', views.task_undone, name='task-undone'),
+    path('edit-task/<str:slug>/', views.edit_task, name='edit-task'),
+    path('delete-task/<str:slug>/', views.delete_task, name='delete-task'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Task
 
 def home(request):
@@ -10,6 +10,32 @@ def add_task(request):
     if request.method == 'POST':
         task = request.POST['add_task']
         Task.objects.create(task=task)
-
     return redirect('todo:home')
 
+def task_complete(request, slug):
+    task = get_object_or_404(Task, slug=slug)
+    task.is_completed = True
+    task.save()
+    return redirect('todo:home')
+
+def task_undone(request, slug):
+    task =get_object_or_404(Task, slug=slug)
+    task.is_completed = False
+    task.save()
+    return redirect('todo:home')
+
+def edit_task(request, slug):
+    get_task = get_object_or_404(Task, slug=slug)
+    if request.method == 'POST':
+        updated_task = request.POST['edit_task']
+        get_task.task = updated_task
+        get_task.save()
+        return redirect('todo:home')
+    else:
+        data = {'get_task': get_task}
+    return render(request, 'todo/edit_task.html', data)
+
+def delete_task(request, slug):
+    delete_task = get_object_or_404(Task, slug=slug)
+    delete_task.delete()
+    return redirect('todo:home')
